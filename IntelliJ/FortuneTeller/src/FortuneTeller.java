e7a5e5e4c693b2198215eb3d071eed23ebf9ce5a
Andrew Struthers <StruthersA@cwu.edu>
2018-11-08 11:49:36 -0800

    Lab7 started

---
+++ IntelliJ/FortuneTeller/src/FortuneTeller.java
@@ -0,0 +1,31 @@
+
+import java.awt.*;   // the "basic" awt functionality
+import javax.swing.*;   // java's awesome swing functionality
+import java.awt.event.*;   // necessary for event listeners
+
+// A FortuneTeller class that extends the JFrame class
+public class FortuneTeller extends JFrame {
+
+    // This class's default, no-argument constructor
+    public FortuneTeller() {
+    }
+
+    // Create the panel, with the necessary components
+    private void buildPanel() {
+    }
+
+    // A private INNER class that implements an event listener.
+    // Recall that the actionPerformed method below
+    // is invoked when the calcButton in the GUI is clicked.
+    private class CalcButtonListener implements ActionListener {
+
+        // The required method, which ActionListner requires
+        public void actionPerformed(ActionEvent e) {
+        }
+
+    }
+
+    // The "main" routine, where it all begins
+    public static void main(String[] args) {
+    }
+}
